76b30537977e2318de434d7c0de37ca869f0e9b2
Naveenkumar V <naveenkumar50192@gmail.com>
2025-10-19 16:52:20 +0530

    Delete ss

--- ss/ecommerce-cart/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProducts, addProduct, seedProducts } = require('../controllers/productController');
-
-router.get('/', getProducts);
-router.post('/', addProduct);
-router.post('/seed', seedProducts); // for seeding sample products
-
-module.exports = router;
